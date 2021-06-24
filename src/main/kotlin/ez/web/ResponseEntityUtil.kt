1ab3e1b68f98f5436b888901fbbfe23ecf9f110a
huangwl <410488219@qq.com>
2021-06-24 17:11:48 +0800

    improve ResponseEntityUtil.kt

--- src/main/kotlin/ez/web/ResponseEntityUtil.kt
+++ src/main/kotlin/ez/web/ResponseEntityUtil.kt
@@ -1,12 +1,12 @@
 package ez.web
 
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
-fun <T>responseEntity(status: HttpStatus = HttpStatus.OK, body: () -> T): ResponseEntity<T> {
-  return ResponseEntity.status(status).body(body())
+fun <T> responseEntity(block: (ResponseEntity.BodyBuilder) -> T): ResponseEntity<T> {
+  val builder = ResponseEntity.ok()
+  return builder.body(block(builder))
 }
 
-fun responseEntity(status: HttpStatus = HttpStatus.OK): ResponseEntity<Void> {
-  return ResponseEntity.status(status).body(null)
+fun responseEntityVoid(block: (ResponseEntity.BodyBuilder) -> Unit): ResponseEntity<Void> {
+  return ResponseEntity.ok().also(block).body(null)
 }
