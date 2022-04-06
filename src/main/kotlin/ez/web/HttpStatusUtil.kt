0679413cfdb7decd7a3664fcece1de0448f56114
huangwl <410488219@qq.com>
2022-04-06 14:38:37 +0800

    update deps

--- src/main/kotlin/ez/web/HttpStatusUtil.kt
+++ src/main/kotlin/ez/web/HttpStatusUtil.kt
@@ -3,5 +3,6 @@ package ez.web
 import org.springframework.http.HttpStatus
 import org.springframework.web.server.ResponseStatusException
 
+@Suppress("unused")
 fun HttpStatus.thr(errMsg: String? = null, cause: Throwable? = null): Nothing =
   throw ResponseStatusException(this, errMsg, cause)
--- src/main/kotlin/ez/web/ResponseEntityUtil.kt
+++ src/main/kotlin/ez/web/ResponseEntityUtil.kt
@@ -2,11 +2,13 @@ package ez.web
 
 import org.springframework.http.ResponseEntity
 
+@Suppress("unused")
 fun <T> responseEntity(block: (ResponseEntity.BodyBuilder) -> T): ResponseEntity<T> {
   val builder = ResponseEntity.ok()
   return builder.body(block(builder))
 }
 
+@Suppress("unused")
 fun responseEntityVoid(block: (ResponseEntity.BodyBuilder) -> Unit): ResponseEntity<Void> {
   return ResponseEntity.ok().also(block).body(null)
 }
